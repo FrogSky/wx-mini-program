9bc10243ab0fcfa00e2c6c087d87f5d222d5c2de
张宁 <zhangning@qding.me>
2020-06-05 17:13:48 +0800

    修改布局

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,18 +1,48 @@
 // pages/my/my.js
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    projectSource: 'https://github.com/liuxuanqiang/wechat-weapp-mall',
+    userListInfo: [ {
+      text: '联系方式',
+      iconClass: 'iconfont icontelephone weui_cell_icon',
+      detail: '15831683090'
+    }, {
+        text: '现居住地',
+        iconClass: 'iconfont iconaddress weui_cell_icon',
+        detail: '北京朝阳区'
+      }, {
+        text: '工作职位',
+        iconClass: 'iconfont iconinfopersonal weui_cell_icon',
+        detail: 'IT精英'
+      }, {
+        text: '兴趣爱好',
+        iconClass: 'iconfont iconlike weui_cell_icon',
+        detail: '篮球、足球、羽毛球'
+      },{
+        text: '联系客服',
+        iconClass: 'iconfont iconsettings weui_cell_icon',
+        detail: '01011111111'
+      }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function() {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo( function( userInfo ) {
+      //更新数据
+      that.setData( {
+        userInfo: userInfo
+      })
+    })
   },
 
   /**
